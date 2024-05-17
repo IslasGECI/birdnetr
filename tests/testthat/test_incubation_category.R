98149c34df5c9b199c20a596591aaf415ba6c1ca
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-05-17 10:31:39 -0700

    🛑🧪 Fail tests

--- tests/testthat/test_incubation_category.R
+++ tests/testthat/test_incubation_category.R
@@ -1,6 +1,8 @@
 describe("Obtain incubation categories", {
   it("Categorizes rows", {
     netting_data <- tibble::tibble(Puntuacion_parche_incubacion = c(0, 1, 1.5, 2, 3, 4, 4.5, 5))
-    get_incubation_category(netting_data)
+    obtained <- get_incubation_category(netting_data)
+    expected_first_category <- 1
+    expect_equal(obtained["incubation_category"][1], expected_first_category)
   })
 })
