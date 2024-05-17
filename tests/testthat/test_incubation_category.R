4bd67159606802bb3ba7904ac6738759bcf55852
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-05-17 11:34:10 -0700

    🛑🧪 Fail tests

--- tests/testthat/test_incubation_category.R
+++ tests/testthat/test_incubation_category.R
@@ -1,10 +1,20 @@
-describe("Obtain incubation categories", {
+decribe("filter rows of interest", {
+  netting_data <- tibble::tibble(
+    Especie = c("Hydrobates homochroa", "Pelecanus occidentalis", "Hydrobates melania", "Hydrobates homochroa"),
+    Isla = c(rep("Todos Santos", 3), "Coronado")
+  )
   it("Filter species of interest", {
-    netting_data <- tibble::tibble(Especie = c("Hydrobates homochroa", "Pelecanus occidentalis", "Hydrobates melania", "Hydrobates homochroa"))
     obtained <- filter_assp_species(netting_data)
     expected_len <- 2
     expect_equal(nrow(obtained), expected_len)
   })
+  it("Filter by Island and species", {
+    obtained <- filter_assp_species_in_todos_santos(netting_data)
+    expected_len <- 1
+    expect_equal(nrow(obtained), expected_len)
+  })
+})
+describe("Obtain incubation categories", {
   it("Categorizes rows", {
     netting_data <- tibble::tibble(Puntuacion_parche_incubacion = c(0, 1, 1.5, 2, 3, 4, 4.5, 5))
     obtained <- get_incubation_category(netting_data)
