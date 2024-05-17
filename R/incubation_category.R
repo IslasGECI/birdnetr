67c05433d484665a05e46b603d21802fa630089d
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-05-17 10:42:30 -0700

    ✅ Pass tests

--- R/incubation_category.R
+++ R/incubation_category.R
@@ -1,4 +1,7 @@
 get_incubation_category <- function(netting_data) {
+  raw_categories <- c(0, 1, 1.5, 2, 3, 4, 4.5, 5)
+  new_categories <- c(1, 2, 2, 3, 3, 4, 4, 1)
+  maped <- plyr::mapvalues(netting_data[["Puntuacion_parche_incubacion"]], from = raw_categories, to = new_categories)
   netting_data |>
-    dplyr::mutate("incubation_category" = 1)
+    dplyr::mutate("incubation_category" = maped)
 }
