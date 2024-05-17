5ba35a7de2624ba03987dbf525cbc9c7eb5e2dcb
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-05-17 15:08:37 -0700

    ✅ Pass tests

---
+++ R/cli.R
@@ -0,0 +1,12 @@
+plot_incubation_categories_proportion <- function(arguments) {
+  netting_data_path <- arguments[["input_data"]]
+  year <- as.character(arguments[["year"]])
+  output_path <- arguments[["output_path"]]
+  netting_data <- readr::read_csv(netting_data_path, show_col_types = FALSE)
+  data_for_plot <- netting_data |>
+    dplyr::filter(stringr::str_sub(Fecha, 4) == year) |>
+    filter_assp_species_in_todos_santos() |>
+    get_incubation_category() |>
+    summarize_by_category_and_month() |>
+    barchart_incubation_categories(year)
+}
