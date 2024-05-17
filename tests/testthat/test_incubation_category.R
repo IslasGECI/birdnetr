a67fac432bbb0b0a132070ec7060cf468eb83208
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-05-17 10:55:17 -0700

    ♻️  Refactor

--- tests/testthat/test_incubation_category.R
+++ tests/testthat/test_incubation_category.R
@@ -2,10 +2,8 @@ describe("Obtain incubation categories", {
   it("Categorizes rows", {
     netting_data <- tibble::tibble(Puntuacion_parche_incubacion = c(0, 1, 1.5, 2, 3, 4, 4.5, 5))
     obtained <- get_incubation_category(netting_data)
-    expected_first_category <- 1
-    expect_equal(obtained[[1, "incubation_category"]], expected_first_category)
-    expected_second_category <- 2
-    expect_equal(obtained[[2, "incubation_category"]], expected_second_category)
+    expected_categories <- c(1, 2, 2, 3, 3, 4, 4, 1)
+    expect_equal(obtained[["incubation_category"]], expected_categories)
   })
   it("Categorizes rows", {
     netting_dirty_data <- tibble::tibble(Puntuacion_parche_incubacion = c(5, 3, NA, 2, 3, NA, 4.5, 5))
