bb23567f5bb38cbcabc893ef14402e7e5949532b
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-05-20 11:42:18 -0700

    🛑🧪 Fail tests

--- tests/testthat/test_cli.R
+++ tests/testthat/test_cli.R
@@ -1,8 +1,8 @@
 describe("Cli for module", {
   it("plot incubation categories proportion", {
     arguments <- list(input_data = "/workdir/tests/data/raw_redeo_iso8601.csv", year = 2022, output_path = "/workdir/tests/data/histograma_incubacion.png")
-    testtools::delete_output_path(arguments["output_path"])
+    testtools::delete_output_file(arguments["output_path"])
     plot_incubation_categories_proportion(arguments)
-    testtools::exist_output_path(arguments["output_path"])
+    testtools::exist_output_file(arguments["output_path"])
   })
 })
