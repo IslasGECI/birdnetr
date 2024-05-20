e34c357b6fd30cc40bf4286093124ad900b70846
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-05-20 12:47:50 -0700

    ♻️  Refactor

--- R/cli.R
+++ R/cli.R
@@ -1,7 +1,8 @@
 #' @export
 plot_incubation_categories_proportion <- function(arguments) {
   netting_data_path <- arguments[["input_data"]]
-  year <- as.character(arguments[["year"]])
+  years_of_interest <- c(2021, 2022)
+  year <- as.character(years_of_interest)
   output_path <- arguments[["output_path"]]
   netting_data <- readr::read_csv(netting_data_path, show_col_types = FALSE)
   data_for_plot <- setup_data_for_incubation_categories_barchart(netting_data, year)
