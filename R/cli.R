ded63ee7daab1b5ae7e3b63edd9e281e8856467e
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-05-20 11:54:21 -0700

    ♻️  Refactor

--- R/cli.R
+++ R/cli.R
@@ -3,11 +3,15 @@ plot_incubation_categories_proportion <- function(arguments) {
   year <- as.character(arguments[["year"]])
   output_path <- arguments[["output_path"]]
   netting_data <- readr::read_csv(netting_data_path, show_col_types = FALSE)
+  data_for_plot <- setup_data_for_incubation_categories_barchart(netting_data, year)
+  barchart_incubation_categories(data_for_plot, year)
+  ggplot2::ggsave(output_path, bg = "transparent")
+}
+
+setup_data_for_incubation_categories_barchart <- function(netting_data, year) {
   data_for_plot <- netting_data |>
     filter_by_year(year) |>
     filter_assp_species_in_todos_santos() |>
     get_incubation_category() |>
     summarize_by_category_and_month()
-  barchart_incubation_categories(data_for_plot, year)
-  ggplot2::ggsave(output_path, bg = "transparent")
 }
