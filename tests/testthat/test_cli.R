f30bd564c21f06037a43db883b298bbcd01b0c8f
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-06-25 13:48:33 -0700

    🛑🧪 Fail tests

--- tests/testthat/test_cli.R
+++ tests/testthat/test_cli.R
@@ -8,4 +8,13 @@ describe("Cli for module", {
     plot_incubation_categories_proportion(arguments)
     expect_true(testtools::exist_output_file(arguments[["output_path"]]))
   })
+  it("Maximum cpue by year for assp", {
+    arguments <- list(
+      "cpue-per-day-path" = "/workdir/tests/data/cpue_per_day_todos_santos_tests.csv",
+      "max-cpue-timeseries-path" = "/workdir/tests/data/max_cpue_timeseries.csv"
+    )
+    testtools::delete_output_file(arguments[["max-cpue-timeseries-path"]])
+    maximum_cpue_timeseries_for_assp(arguments)
+    expect_true(testtools::exist_output_file(arguments[["max-cpue-timeseries-path"]]))
+  })
 })
