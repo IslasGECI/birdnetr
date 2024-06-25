b15c1e9539a86a109a48d7bbe0814b8362f68e8c
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-06-25 13:35:39 -0700

    ✅ Pass tests

--- R/cpue_timeseries.R
+++ R/cpue_timeseries.R
@@ -1,3 +1,6 @@
 maximum_cpue_by_year <- function(cpue_data) {
-
+  cpue_data |>
+    dplyr::group_by(Temporada) |>
+    dplyr::filter(Especie == "Hydrobates homochroa") |>
+    dplyr::summarize(max_cpue = max(CPUE_total))
 }
