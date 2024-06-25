66dafa31aa5cb9c6266f258d179d9588f18b56d0
Mario Villasante <mario.villasante@islas.org.mx>
2024-06-25 14:16:00 -0700

    🚸➕ add domain specific options for cli

--- R/optparse.R
+++ R/optparse.R
@@ -2,6 +2,8 @@
 get_domain_specific_options <- function() {
   data_path <- geci.optparse::character_option(c("-i", "--input_data"), default = "/workdir/", help = "File input")
   output_path <- geci.optparse::character_option(c("-o", "--output_path"), default = "/workdir/", help = "File output")
-  option_names <- c(data_path, output_path)
+  cpue_per_day_path <- geci.optparse::character_option(c("-c", "--cpue-per-day-path"), default = "/workdir/", help = "CPUE by day for all seasons path")
+  max_cpue_timeseries_path <- geci.optparse::character_option(c("-m", "--max-cpue-timeseries-path"), default = "/workdir/", help = "Max CPUE timeseries path")
+  option_names <- c(data_path, output_path, cpue_per_day_path, max_cpue_timeseries_path)
   geci.optparse::get_options_from_vec(option_names)
 }
