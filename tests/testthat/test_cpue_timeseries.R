88606090c31ad5c984bd53bd8aa4bcf21f27553d
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-06-25 13:31:42 -0700

    🛑🧪 Fail tests

--- tests/testthat/test_cpue_timeseries.R
+++ tests/testthat/test_cpue_timeseries.R
@@ -2,9 +2,12 @@ describe("Write CPUE time series", {
   cpue_data <- tibble::tibble(
     Temporada = c(rep(2010, 2), rep(2011, 2), rep(2013, 2)),
     CPUE_total = c(0, 1, 2, 3, 4, 5),
-    Especie = c(rep("Hydrobates", 3), rep("Melania", 2), "Hydrobates")
+    Especie = c(rep("Hydrobates homochroa", 3), rep("Hydrobates", 2), "Hydrobates homochroa")
   )
   it("select maximum cpue by year", {
-    maximum_cpue_by_year(cpue_data)
+    obtained <- maximum_cpue_by_year(cpue_data)
+    obtained_rows <- nrow(obtained)
+    expected_rows <- 3
+    expect_equal(obtained_rows, expected_rows)
   })
 })
